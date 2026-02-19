c72f5afe8471560ac64f7026d2bdbf9e5c793844
hamid <111028733+hamidriaz1998@users.noreply.github.com>
2026-02-19 10:42:21 +0500

    Add greeting screen

--- our_package/app/src/main/java/com/example/our_package/MainActivity.kt
+++ our_package/app/src/main/java/com/example/our_package/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.our_package
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.view.View.VISIBLE
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val editText = findViewById<EditText>(R.id.editTextText)
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            val name = editText.text
+            if (!name.isEmpty()){
+                textView.text = "Hello, $name"
+                textView.visibility = VISIBLE
+            } else{
+                textView.text = "Invalid name. Try again"
+                textView.visibility = VISIBLE
+                textView.setTextColor(Color.RED)
+            }
         }
     }
 }
